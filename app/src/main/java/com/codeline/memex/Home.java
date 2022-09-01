57dbf13429395bd2f418feb5eb7c0181f109f0b3
HermonYurandaCruz <hermondacruz73@gmail.com>
2022-09-01 21:07:14 +0200

    primeiro commit

---
+++ app/src/main/java/com/codeline/memex/Home.java
@@ -0,0 +1,64 @@
+package com.codeline.memex;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
+
+        bottomNavigationView=findViewById(R.id.bottonNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.conteinernavagation,homeFragment);
+
+    }
+    HomeFragment homeFragment=new HomeFragment();
+    MemeirosFragment memeirosFragment=new MemeirosFragment();
+
+    RankingFragment rankingFragment=new RankingFragment();
+    NotificaoessFragment notificaoessFragment=new NotificaoessFragment();
+    DefinicoesFragment definicoesFragment=new DefinicoesFragment();
+
+
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.conteinernavagation, homeFragment).commit();
+                return true;
+
+            case R.id.memeiros:
+                getSupportFragmentManager().beginTransaction().replace(R.id.conteinernavagation,memeirosFragment).commit();
+                return true;
+
+
+            case R.id.definicoes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.conteinernavagation,definicoesFragment).commit();
+                return true;
+
+            case R.id.ranking:
+                getSupportFragmentManager().beginTransaction().replace(R.id.conteinernavagation,rankingFragment).commit();
+                return true;
+
+            case R.id.notificacoes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.conteinernavagation,notificaoessFragment).commit();
+                return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/codeline/memex/TelaLogin.java
@@ -0,0 +1,14 @@
+package com.codeline.memex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_login);
+    }
+}
