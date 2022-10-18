52572b24f7bd8833080cbe9cceb19ce73ccb7683
Stanley <Nhampossastanley@gmail.com>
2022-10-18 18:17:51 +0200

    Adicionando autenticacao com email e senha usando firebase

--- app/src/main/java/com/codeline/memex/TelaCriarConta.java
+++ app/src/main/java/com/codeline/memex/TelaCriarConta.java
@@ -1,19 +1,76 @@
 package com.codeline.memex;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class TelaCriarConta extends AppCompatActivity {
+
+    private EditText edtEmail;
+    private EditText edtSenha;
+    private Button btnCadastrar;
+//    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_criar_conta);
         getSupportActionBar().hide();
+        findById();
+//        mAuth = FirebaseAuth.getInstance();
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registrar();
+            }
+        });
+
     }
 
+    public void findById(){
+        edtEmail=findViewById(R.id.et_emailNew);
+        edtSenha=findViewById(R.id.et_senhaNew);
+        btnCadastrar=findViewById(R.id.bt_login);
+    }
+
+
+    public void registrar(){
+//        String email = edtEmail.getText().toString();
+//        String password = edtSenha.getText().toString();
+//        mAuth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithCustomToken:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            Toast.makeText(TelaCriarConta.this, "Tudo nice", Toast.LENGTH_LONG).show();
+//                            //updateUI(user);
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+//                            Toast.makeText(TelaCriarConta.this, "Authentication failed." + task.getException(),
+//                                    Toast.LENGTH_SHORT).show();
+//                            //updateUI(null);
+//                        }
+//                    }
+//                });
+    }
 
 
 }
--- app/src/main/java/com/codeline/memex/TelaLogin.java
+++ app/src/main/java/com/codeline/memex/TelaLogin.java
@@ -1,24 +1,39 @@
 package com.codeline.memex;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class TelaLogin extends AppCompatActivity {
     private Button bt_entrar;
     private TextView tv_criarConta;
     private TextView tv_EsqueceuSenha;
+    private EditText edt_email;
+    private EditText edt_senha;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
         getSupportActionBar().hide();
     IniciarNovoCadastro();
-
+        mAuth = FirebaseAuth.getInstance();
     tv_criarConta.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -31,10 +46,11 @@ public class TelaLogin extends AppCompatActivity {
     tv_EsqueceuSenha.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Intent intent = new Intent(TelaLogin.this, ActivityRecuperarSenha.class);
-            startActivity(intent);
+//            Intent intent = new Intent(TelaLogin.this, ActivityRecuperarSenha.class);
+//            startActivity(intent);
         }
     });
+
 
 
 
@@ -43,7 +59,7 @@ public class TelaLogin extends AppCompatActivity {
     bt_entrar.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-        AbrirTelaHome();
+            iniciarSessao();
         }
     });
     }
@@ -59,6 +75,8 @@ public class TelaLogin extends AppCompatActivity {
 
 
     private  void IniciarNovoCadastro(){
+        edt_email = findViewById(R.id.usuarioLogin);
+        edt_senha = findViewById(R.id.senhaLogin);
         bt_entrar=findViewById(R.id.bt_login);
         tv_criarConta=findViewById(R.id.tv_criar_conta);
         tv_EsqueceuSenha = findViewById(R.id.tv_esquecu_senha);
@@ -70,5 +88,30 @@ public class TelaLogin extends AppCompatActivity {
         tv_recuperarSenha=findViewById(R.id.Tv_recuperarSenha);
 
         */
+    }
+
+    private void iniciarSessao(){
+        String email = edt_email.getText().toString();
+        String password = edt_senha.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCustomToken:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(TelaLogin.this, "Tudo nice", Toast.LENGTH_LONG).show();
+                            AbrirTelaHome();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                            Toast.makeText(TelaLogin.this, "Dados incorrectos",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
     }
 }
