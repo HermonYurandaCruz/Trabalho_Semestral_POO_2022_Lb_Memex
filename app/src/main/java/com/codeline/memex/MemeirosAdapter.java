345d081bc08c058641acb95cee4de51693f358a2
Stanley Nhampossa <99397040+StanleyNhampossa@users.noreply.github.com>
2022-10-27 22:39:01 +0200

    corrigindo bugs

---
+++ app/src/main/java/com/codeline/memex/MemeirosAdapter.java
@@ -0,0 +1,50 @@
+package com.codeline.memex;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.bumptech.glide.Glide;
+import com.codeline.memex.model.Memeiros;
+
+import java.util.List;
+
+public class MemeirosAdapter extends RecyclerView.Adapter<MemeirosHolder> {
+
+    Context context;
+    List<Memeiros> memeiros;
+
+    public MemeirosAdapter(Context context, List<Memeiros> memeiros) {
+        this.context = context;
+        this.memeiros = memeiros;
+    }
+
+    @NonNull
+    @Override
+    public MemeirosHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_usuario, parent, false);
+        return new MemeirosHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MemeirosHolder holder, int position) {
+
+        Memeiros memeiros = this.memeiros.get(position);
+        if(memeiros.getUrl_foto_perfil() != null)
+            Glide.with(context).load(memeiros.getUrl_foto_perfil()).into(holder.imagem);
+        holder.tv_nomeUsuario.setText(memeiros.getNome_usuario());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return memeiros.size();
+    }
+
+
+}
---
+++ app/src/main/java/com/codeline/memex/MemeirosHolder.java
@@ -0,0 +1,24 @@
+package com.codeline.memex;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MemeirosHolder extends RecyclerView.ViewHolder {
+    TextView tv_numeroSeguidores,tv_numeroPostes,tv_nomeUsuario;
+    Button bt_seguir;
+    ImageView imagem;
+
+    public MemeirosHolder(@NonNull View itemView) {
+        super(itemView);
+        tv_nomeUsuario = itemView.findViewById(R.id.tv_nomeSeguir);
+        imagem = itemView.findViewById(R.id.iv_perfil_usuario);
+        itemView.findViewById(R.id.tv_numeroPostes);
+        itemView.findViewById(R.id.tv_numeroSeguidores);
+        itemView.findViewById(R.id.bt_seguir);
+    }
+}
